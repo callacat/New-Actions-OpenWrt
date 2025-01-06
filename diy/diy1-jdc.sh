62bfbd400489c431578c6ff626ddfc68a82471bf
callacat <wang892947707@hotmail.com>
2025-01-06 11:30:02 +0800

    切换内核5.15

--- diy/diy1-jdc.sh
+++ diy/diy1-jdc.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 修改内核
+sed -i 's/^KERNEL_PATCHVER:=/KERNEL_PATCHVER:=5.15/' target/linux/ramips/Makefile
